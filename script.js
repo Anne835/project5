1a395b5189cb96f0af120ffb902f5b08f439edf2
Anne835 <annmallum83@gmail.com>
2025-03-17 22:17:08 +0100

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".menu-btn").addEventListener("click", function(){
+    document.querySelector(".nav-links").classList.toggle("active");
+});
+document.querySelector(".close-btn").addEventListener("click", function(){
+    document.querySelector(".nav-links").classList.toggle("active");
+});
